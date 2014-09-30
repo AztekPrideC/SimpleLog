6636408981e978f41f8f71d5f71dd4f3fb3963e0
Roberto <rober@null.net>
2014-09-30 08:27:19 -0500

    Metodo Registro

---
+++ SimpleLog/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLog
+{
+    public static class Log
+    {
+        public static void Registro(DateTime fecha, int status, String operacion, String descripcion)
+        {
+            File.AppendAllText("log.txt", fecha+" | "+status+" "+operacion+" "+descripcion);
+        }
+    }
+}
